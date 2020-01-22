71a9e323a45c7568eee9fe50046700f22418b743
cameron <chogue0189@gmail.com>
2020-01-21 20:54:18 -0600

    Find by value

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -110,7 +110,18 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if(someJobs.isEmpty()) {
+            System.out.println("No Jobs Found.");
+        } else {
+            for (HashMap<String, String> entry : someJobs) {
+                System.out.println("*****");
+                for (HashMap.Entry<String, String> job : entry.entrySet()) {
+                    System.out.println(job.getKey() + ": " + job.getValue());
+                }
+            }
+            System.out.println("*****");
 
-        System.out.println("printJobs is not implemented yet");
+        }
+
     }
 }
